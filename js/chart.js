6960b00b89e8d9f725f796e5fd4bbc603cf18063
Ritom Sonowal <ritomsonowal99@gmail.com>
2020-03-21 19:14:28 +0530

    Add first graph

--- js/chart.js
+++ js/chart.js
@@ -1,96 +1,129 @@
-// function generateLabels(data, country) {                       
-//     var labels = [];
+let colors = ['rgb(216, 36, 8, 0.7)','rgb(17, 145, 224, 0.7)', 'rgb(224, 221, 23, 0.7)'];
+var i = 0;
+var n = 3;
 
-//     data[country].forEach(({ date, confirmed, recovered, deaths }) =>
-//         labels.push(date));
-    
-//     return labels;
-// }
-
-// function generateData(data, country) {                       
-//     var data = [];
-
-//     data[country].forEach(({ date, confirmed, recovered, deaths }) =>
-//         data.push(confirmed));
-    
-//     return data;
-// }
-
-
-function generateLabelsFromTable()
-{                       
+function getLabels(data, country) {
     var labels = [];
+    // console.log(data);
+    // console.log(data);
 
-    var rows = $("tr");
-    rows.each(function(index){
-        if (index != 0)  // we dont need first row of table
-        {
-            var cols = $(this).find("td");      
-            labels.push(cols.first().text());                           
-        }
-    });
+    data[country].forEach(({ date, confirmed, recovered, deaths }) =>
+    labels.push(date));
+
     return labels;
 }
-function generateDataSetsFromTable()
-{
-    var data;
-    var datasets = [];
-    var rows = $("tr");
-    rows.each(function(index){
-        if (index != 0) // we dont need first row of table
-        {
-            var cols = $(this).find("td");  
-            var data = [];
-            cols.each(function(innerIndex){
-                if (innerIndex!=0) // we dont need first columns of the row                 
-                    data.push($(this).text());                                          
-            });
-						var colors = [];
-colors.push(                        
-{
-        fillColor : "rgba(95,137,250,0.5)",
-        strokeColor : "rgba(95,137,250,0.9)",
-        highlightFill: "rgba(95,137,250,0.75)",
-        highlightStroke: "rgba(95,137,250,1)"
-});
-colors.push(                        
-{
-        fillColor : "rgba(245,75,75,0.5)",
-        strokeColor : "rgba(245,75,75,0.8)",
-        highlightFill : "rgba(245,75,75,0.75)",
-        highlightStroke : "rgba(245,75,75,1)"
-});
-colors.push(                        
-{
-        fillColor : "rgba(98,223,114,0.5)",
-        strokeColor : "rgba(98,223,114,0.8)",
-        highlightFill : "rgba(98,223,114,0.75)",
-        highlightStroke : "rgba(98,223,114,1)",
-});
 
-            var dataset = 
-            {
-            fillColor : colors[index%3].fillColor,
-                strokeColor : colors[index%3].strokeColor,
-                highlightFill: colors[index%3].highlightFill,
-                highlightStroke: colors[index%3].highlightStroke,
-                data : data
+function getConfirmed(data, country) {
+    var dataset = [];
+
+    data[country].forEach(({ date, confirmed, recovered, deaths }) =>
+    dataset.push(confirmed));
+
+    return dataset;
+}
+
+function getCountries(data) {
+    var countries = [];
+
+    for (var key of Object.keys(data)) {
+        countries.push(key);
+    }
+
+    return countries;
+}
+
+function getChartData(data, country) {
+    var chartData = {
+        labels: getLabels(data, country),
+        datasets: [{
+            label: country,
+            data: getConfirmed(data, country),
+            borderWidth: 1,
+            lineTension: 0,
+            order: 0,
+            fill: false,
+            borderColor: colors[i]
+            }]
+        };
+        
+    i = (i+1)%n;
+    console.log(chartData);
+    return chartData;
+}
+
+
+function renderChart1(chartData) {
+    var ctx = document.getElementById('chart1');   
+
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: chartData,
+        options: {
+            title: {
+                display: true,
+                text: 'Confirmed cases Graph'
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+            layout: {
+                padding: {
+                    left: 20,
+                    right: 20,
+                    top: 0,
+                    bottom: 0
+                }
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false,
+                        suggestedMin: 500,
+                        suggestedMax: 1000
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Confirmed Cases"
+                    }
+                }]
             }
-
-            datasets.push(dataset);
-
         }
-    });
-    return datasets;
+    }); 
+
+    setTimeout(function() { myChart.update(); },1000); 
+
+    return myChart;
 }
-var barChartData = {
-    labels : generateLabelsFromTable(),
-    datasets : generateDataSetsFromTable()
-};
-function drawChart() {
-            var ctx = document.getElementById("canvas_bar").getContext("2d");
-            window.myBar = new Chart(ctx).Bar(barChartData, {
-                responsive : true
-            });
+
+
+function addData(chart, jsonData, country) {
+    var newDataset = {
+        label: country,
+        data: getConfirmed(jsonData, country),
+        borderWidth: 1,
+        lineTension: 0,
+        order: 0,
+        fill: false,
+        borderColor: colors[i]
+        };
+
+    i = (i+1)%n;
+
+    chart.data.datasets.push(newDataset);
+    chart.update();
+    // console.log(newDataset);
+
+    console.log("Updated!");
 }
-drawChart();
+
+function addCountryList(data) {
+
+    var select = $('<select id="country-list"/>');
+
+    for(var val in data) {
+        $('<option />', {value: val, text: val}).appendTo(select);
+    }
+    
+    select.appendTo('#add-country');
+
+
+}
--- js/init.js
+++ js/init.js
@@ -1,79 +1,39 @@
-var dates = [];
-var cases = [];
+var jsonData;
+var data;
+
+var url = "https://pomber.github.io/covid19/timeseries.json"; 
+var addedCountries = ["China", "India", "Italy"];
 
 $(document).ready(function () {
-    // fetch("https://pomber.github.io/covid19/timeseries.json")
-    // .then(response => response.json())
-    // .then(data => {
-    //     data["China"].forEach(({ date, confirmed, recovered, deaths }) =>
-    //     dates.push(date)
-    //     )
-    // });
 
-    $.get("https://pomber.github.io/covid19/timeseries.json", function(data, status){
-        // alert("Data: " + data + "\nStatus: " + status);
-        // console.log(data["China"]);
+    $.ajax({
+        'async': false,
+        'type': "GET",
+        'global': false,
+        'dataType': 'json',
+        'url': url,
+        'success': function (jsonData) {
+            data = jsonData;
+        }
+    });
 
-        data["China"].forEach(({ date, confirmed, recovered, deaths }) =>
-        dates.push(date));
+    console.log(data);
 
-        data["China"].forEach(({ date, confirmed, recovered, deaths }) =>
-        cases.push(confirmed));
-      });
+    // Render Chart 1
+    chart1 = renderChart1(getChartData(data, "China"));
+    
+    addData(chart1, data, "India");
+    addData(chart1, data, "Italy");
 
-    // console.log(dates);
-    // console.log(cases);
+    $("#country-list-btn").click(function(){
+        $("#add-country").toggle();
+    });
+
+    addCountryList(data);
+
+    $("#add-country-btn").click(function(){
+        addData(chart1, data, $('#country-list').val());
+        console.log("Added new country!");
+    });
 });
 
-// var cases = [300, 500, 2300, 2500, 3000 ];
-// var dates = [2,3,4,6,7]
-
-
-window.onload = function () {
-    var ctx = document.getElementById('chart1');
-    console.log(cases);
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: dates,
-            datasets: [{
-                label: 'Mainland China',
-                data: cases,
-                borderWidth: 1,
-                lineTension: 0,
-                order: 0
-            }]
-        },
-        options: {
-            title: {
-                display: true,
-                text: 'Confirmed cases Graph'
-            },
-            responsive: true,
-            maintainAspectRatio: false,
-            layout: {
-                padding: {
-                    left: 20,
-                    right: 20,
-                    top: 0,
-                    bottom: 0
-                }
-            },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: false,
-                        suggestedMin: 500,
-                        suggestedMax: 1000
-                    }
-                }],
-                scaleLabel: [{
-                    display: true,
-                    labelString: "Confirmed Cases"
-                }]
-            }
-        }
-    });    
-
-    setTimeout(function() { myChart.update(); },1000);
-}
