60d38774579a75bec7d0e5793def7ef7dca4afc5
Ritom Sonowal <ritomsonowal99@gmail.com>
2020-03-23 11:15:45 +0530

    Fix adding repeated countries issue

--- js/init.js
+++ js/init.js
@@ -3,8 +3,8 @@ var data;
 var population;
 
 var covidURL = "https://pomber.github.io/covid19/timeseries.json"; 
-var addedCountries = ["China", "India", "Italy"];
-var addedbarCountries = ["China", "India", "Italy", "United Kingdom", "US", "Spain"];
+var addedlineCountries = ["China", "India", "Italy", "United Kingdom", "US", "Spain", "France"];
+var addedbarCountries = ["China", "India", "Italy", "United Kingdom", "US", "Spain", "France"];
 
 $(document).ready(function () {
 
@@ -24,8 +24,13 @@ $(document).ready(function () {
     // Render Chart 1
     chart1 = renderChart1(getlineData(data, "China"));
     
-    addLineData(chart1, data, "India");
-    addLineData(chart1, data, "Italy");
+    // addLineData(chart1, data, "India");
+    // addLineData(chart1, data, "Italy");
+
+    let i=0;
+    for (i=0;i<addedlineCountries.length;i++) {
+        addLineData(chart1,data,addedlineCountries[i]);
+    }
 
     $("#country-list-btn").click(function(){
         $("#add-country").toggle();
@@ -36,8 +41,13 @@ $(document).ready(function () {
     addCountryList2(data);
 
     $("#add-country-btn").click(function(){
-        addLineData(chart1, data, $('#country-list').val());
-        console.log("Added new country!");
+        if (addedlineCountries.includes($('#country-list').val())) {
+            alert("Already added country!");
+        } else {
+            addLineData(chart1, data, $('#country-list').val());
+            addedlineCountries.push($('#country-list').val());
+            console.log("Added new country!");
+        }
     });
 
     // Render Chart 2
@@ -55,8 +65,13 @@ $(document).ready(function () {
     });
 
     $("#add-country-btn2").click(function(){
-        addbarData(chart2, data, $('#country-list2').val());
-        console.log("Added new country!");
+        if (addedbarCountries.includes($('#country-list2').val()) ) {
+            alert("Already added country!");
+        } else {
+            addbarData(chart2, data, $('#country-list2').val());
+            addedbarCountries.push($('#country-list2').val());
+            console.log("Added new country!");
+        }
     });
 
 });
