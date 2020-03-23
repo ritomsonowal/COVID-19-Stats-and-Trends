e509e55b8c2cb0f65139627cee5203f5abacfcdf
Ritom Sonowal <ritomsonowal99@gmail.com>
2020-03-23 11:05:04 +0530

    Update graph 1

--- js/chart.js
+++ js/chart.js
@@ -1,9 +1,10 @@
-let colors = ['rgb(216, 36, 8, 0.7)','rgb(230, 171, 26)','rgb(17, 145, 224, 0.7)', 'rgb(224, 221, 23, 0.7)'];
+let colors = ['rgb(216, 36, 8, 0.7)','rgb(230, 171, 26)','rgb(17, 145, 224, 0.7)', 'rgb(224, 221, 23, 0.7)', 'rgb(117, 121, 127)'];
 var i = 0;
-var n = 4;
+var n = 5;
 
 var popURL = "https://restcountries.eu/rest/v2/all?fields=name;nativeName;population";
 var popData;
+var days = 100;
 
 $(document).ready(function () {
 
@@ -21,22 +22,71 @@ $(document).ready(function () {
 
 function getLabels(data, country) {
     var labels = [];
+    var dates = [];
     // console.log(data);
     // console.log(data);
 
     data[country].forEach(({ date, confirmed, recovered, deaths }) =>
-    labels.push(date));
+    dates.push(date));
 
-    return labels.slice(-27,-1);;
+    let i=0;
+    var day = 1;
+
+    for (i=0;i<dates.length;i++) {
+        if (data[country][i]["confirmed"] >= days) {
+            labels.push(day);
+            day += 1;
+        }
+    }
+
+    console.log(labels);
+
+    return labels;
 }
 
 function getConfirmed(data, country) {
     var dataset = [];
 
-    data[country].forEach(({ date, confirmed, recovered, deaths }) =>
-    dataset.push(confirmed));
+    // data[country].forEach(({ date, confirmed, recovered, deaths }) =>
+    // dataset.push(confirmed));
+    let i=0;
+    for (i=0;i<data[country].length;i++) {
+        if (data[country][i]["confirmed"] >= days) {
+            dataset.push(data[country][i]["confirmed"]);
+        }
+    }
 
-    return dataset.slice(-27,-1);
+    return dataset;
+}
+
+function getDeaths(data, country) {
+    var dataset = [];
+
+    // data[country].forEach(({ date, confirmed, recovered, deaths }) =>
+    // dataset.push(confirmed));
+    let i=0;
+    for (i=0;i<data[country].length;i++) {
+        if (data[country][i]["confirmed"] >= days) {
+            dataset.push(data[country][i]["deaths"]);
+        }
+    }
+
+    return dataset;
+}
+
+function getRecovered(data, country) {
+    var dataset = [];
+
+    // data[country].forEach(({ date, confirmed, recovered, deaths }) =>
+    // dataset.push(confirmed));
+    let i=0;
+    for (i=0;i<data[country].length;i++) {
+        if (data[country][i]["confirmed"] >= days) {
+            dataset.push(data[country][i]["recovered"]);
+        }
+    }
+
+    return dataset;
 }
 
 function getPopulation(country) {
@@ -61,17 +111,28 @@ function getCountries(data) {
 }
 
 function getLineDataset(data, country) {
-    var confirmedCases = getConfirmed(data, country);
+    // var confirmedCases = getConfirmed(data, country);
     var population = getPopulation(country);
     if (population == false) {
         population = 1;
         console.log("Could not retrieve population for " + country);
     }
+    // var linedata = [];
+    // let i=0;
+
+    // for (i=0;i<confirmedCases.length;i++) {
+    //     linedata.push(confirmedCases[i]/population);
+    // } return linedata;
+
+    var confirmedCases = getConfirmed(data, country);
+    var deaths = getDeaths(data, country);
+    var recovered = getRecovered(data, country);
+
     var linedata = [];
     let i=0;
 
     for (i=0;i<confirmedCases.length;i++) {
-        linedata.push(confirmedCases[i]/population);
+        linedata.push((confirmedCases[i]-recovered[i]-deaths[i]));
     } return linedata;
 }
 
@@ -82,7 +143,7 @@ function getlineData(data, country) {
         datasets: [{
             label: country,
             data: getLineDataset(data, country),
-            borderWidth: 1,
+            borderWidth: 3,
             lineTension: 0,
             order: 0,
             fill: false,
@@ -91,7 +152,7 @@ function getlineData(data, country) {
         };
         
     i = (i+1)%n;
-    // console.log(lineData);
+    console.log(lineData);
     return lineData;
 }
 
@@ -105,8 +166,11 @@ function renderChart1(lineData) {
         options: {
             title: {
                 display: true,
-                text: '(Confirmed cases)/Population Graph'
+                text: 'Active Cases Graph'
             },
+            tooltips: {
+                mode: 'nearest'
+            },            
             responsive: true,
             maintainAspectRatio: false,
             layout: {
@@ -120,13 +184,21 @@ function renderChart1(lineData) {
             scales: {
                 yAxes: [{
                     ticks: {
-                        beginAtZero: false,
-                        suggestedMin: 500,
-                        suggestedMax: 1000
+                        beginAtZero: false
                     },
                     scaleLabel: {
                         display: true,
-                        labelString: "(Confirmed Cases)/Population"
+                        labelString: "Active Cases"
+                    }
+                }],
+                xAxes: [{
+                    ticks: {
+                        beginAtZero: false,
+                        stepSize: 5
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Days since 100 confirmed"
                     }
                 }]
             }
@@ -142,8 +214,8 @@ function renderChart1(lineData) {
 function addLineData(chart, jsonData, country) {
     var newDataset = {
         label: country,
-        data: getConfirmed(jsonData, country),
-        borderWidth: 1,
+        data: getLineDataset(jsonData, country),
+        borderWidth: 3,
         lineTension: 0,
         order: 0,
         fill: false,
@@ -195,7 +267,7 @@ function renderChart2(countries, barData) {
                 barThickness: 30,
                 label: 'Percentage Increase in Confirmed Cases',
                 data: barData,
-                borderWidth: 1,
+                borderWidth: 5,
                 backgroundColor: colors[0]
                 }]
             },
